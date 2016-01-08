5f1f7163334bf70c2ff2e5b264813989f3e93f47
kalsipp <martin.favre22@gmail.com>
2016-01-08 07:01:07 +0100

    Removed surface, added openGL. Fixed keyinputs

--- src/camera.cpp
+++ src/camera.cpp
@@ -5,9 +5,6 @@ Camera::Camera(){
 	m_log = new Logger(m_logfile_name);
 	m_log->log("Initializing of " + m_window_name);
 
-	m_screen_width = 640;
-	m_screen_height = 480;
-
 	if(SDL_Init(SDL_INIT_VIDEO) < 0){
 		m_log->log( "SDL could not initialize! SDL_Error: " + std::string(SDL_GetError()) );
 	}else{
@@ -17,17 +14,54 @@ Camera::Camera(){
 			SDL_WINDOWPOS_UNDEFINED,
 			m_screen_width, 
 			m_screen_height, 
-			SDL_WINDOW_SHOWN);
+			SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 		if(m_sdl_window  == nullptr){
 			m_log->log( "SDL could not initialize! SDL_Error: " + std::string(SDL_GetError()) );
 		}else{
-			m_sdl_surface = SDL_GetWindowSurface(m_sdl_window);
+			m_sdl_glcontext = SDL_GL_CreateContext(m_sdl_window);
+			if(m_sdl_glcontext == 0){
+				m_log->log("OpenGL context could not be created! SDL Error: " + std::string(SDL_GetError()));
+			}else{
+				if(SDL_GL_SetSwapInterval(1) < 0){
+					m_log->log("Warning: Unable to set VSync! SDL Error: " + std::string(SDL_GetError()) );
+				}
+
+				if(!init_gl()){
+					m_log->log("Unable to initialize OpenGL!");
+				}
+			}
+			//m_sdl_surface = SDL_GetWindowSurface(m_sdl_window);
 		}
 	}
-
+	m_successful_init = true;
 	m_log->log("Done initializing of " + m_window_name);
 }
+bool Camera::init_gl(){
+	bool success = true;
+	GLenum error = GL_NO_ERROR;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	error = glGetError();
+	if(error != GL_NO_ERROR){
+		success = false;
+	}
 
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	error = glGetError();
+	if(error != GL_NO_ERROR){
+		success = false;
+	}
+
+	glClearColor(0.f, 0.f, 0.f, 1.f);
+
+	error = glGetError();
+	if(error != GL_NO_ERROR){
+		success = false;
+	}
+
+	return success;
+}
 Camera::~Camera(){
 
 	m_log->log("Running destructor of " + m_window_name);
@@ -38,10 +72,29 @@ Camera::~Camera(){
 	delete m_log;
 
 }
+bool Camera::init_successful(){
+	return m_successful_init;
+}
 void Camera::render(){
+    glClear( GL_COLOR_BUFFER_BIT );
+    
+    //Render quad
 
-	SDL_FillRect(m_sdl_surface, nullptr, SDL_MapRGB(m_sdl_surface->format, 255, 255, 255));
-	SDL_UpdateWindowSurface(m_sdl_window);
+        glRotatef(0.4f,0.0f,1.0f,0.0f);    // Rotate The cube around the Y axis
+        glRotatef(0.2f,1.0f,1.0f,1.0f);
+        glColor3f(0.0f,1.0f,0.0f); 
+
+        glBegin( GL_QUADS );
+            glVertex2f( -0.5f, -0.5f );
+            glVertex2f( 0.5f, -0.5f );
+            glVertex2f( 0.5f, 0.5f );
+            glVertex2f( -0.5f, 0.5f );
+        glEnd();
+
+}
+
+void Camera::update(){
+	SDL_GL_SwapWindow(m_sdl_window);
 
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,39 @@
 #include <SDL_opengl.h>
 
 #include "camera.hpp"
-
+#include "logger.hpp"
 
 int main(){
 	Camera g;
-	while(true){
-		g.render();
-		SDL_Delay(2000);
-		break;
+	Logger mylog("mainlog.txt");
+	if(g.init_successful()){
+		bool running = true;
+		SDL_Event events;
+		SDL_StartTextInput();
+		//mylog.log(SDL_TEXTINPUT);
+		while(running){
+
+			while(SDL_PollEvent(&events) != 0){ 
+				switch(events.type){
+
+					case SDL_QUIT:
+						running = false;
+						break;
+					case SDL_TEXTINPUT:
+					int x = 0, y = 0;
+					SDL_GetMouseState(&x, &y); //doesn't use this atm contains mousepos
+
+					mylog.log("keys " + std::string(events.text.text));// << events.text.text[0] << std::endl;
+					
+					if(events.text.text[0] == 'q'){
+						running = false;
+					}
+				}
+			}
+
+			g.render();
+			g.update();
+		}
+		SDL_StopTextInput();
 	}
 }
